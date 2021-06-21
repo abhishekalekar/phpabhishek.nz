d5644164b6e6eb1b1516722a88fc83fb683ae05b
abhishekalekar <abhishekalekar@gmail.com>
2021-06-21 14:38:49 +1200

    updated correct files

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,56 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Abhishek Alekar | Full Stack Developer | PHPAbhishek', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hi, my name is ',
+  name: 'Abhishek Alekar',
+  subtitle: "I'm Full Stack Developer",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'profile-1.jpg',
+  paragraphOne: 'Knowledgeable of back end and front end development requirements.',
+  paragraphTwo: 'Able to handle any part of the process with Well-qualified Full Stack Developer familiar with a wide range of programming utilities and languages.',
+  paragraphThree: 'Collaborative team player with excellent technical abilities offering 7 years of related experience.',
+  resume: 'https://gitconnected.com/abhishekalekar/resume', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project-faq.jpg',
+    title: 'FAQ-Accordion-Card',
+    info: 'Challenge by FrontEnd Mentor',
+    info2: 'coded by Abhishek Alekar',
+    url: 'https://abhishekalekar.github.io/FAQ-Accordion-Card/',
+    repo: 'https://github.com/abhishekalekar/FAQ-Accordion-Card', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'project-bitcoin.jpg',
+    title: 'Bitcoin Price Index ',
+    info: 'Bitcoin Price index build using HTML | CSS',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://abhishekalekar.github.io/BitCoinPriceIndex',
+    repo: 'https://github.com/abhishekalekar/BitCoinPriceIndex', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'react-portfolio.jpg',
+    title: 'React based Portfolio',
+    info: 'Porfolio developed using gitconnected API and React as FrontEnd',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://abhishekalekar.github.io/create-react-app-portfolio',
+    repo: 'https://github.com/abhishekalekar/create-react-app-portfolio', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +59,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'abhishekalekar[at]gmail.com',
 };
 
 // FOOTER DATA
@@ -68,7 +68,7 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/abhishekalekar',
     },
     {
       id: nanoid(),
@@ -78,17 +78,17 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://linkedin.com/in/abhishekalekar',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/abhishekalekar',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
